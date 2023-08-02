35c4f15f70618280f0f09b179fc229bb9e99d8bf
Al Napp <al@alnapp.com>
2023-08-02 12:45:46 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,15 +1,73 @@
-// create web server to handle requests
-// http://nodejs.org/api/http.html#http_http
-var http = require('http');
-
 // create web server
-var server = http.createServer(function(req, res){
-  // handle incoming requests here..
-  console.log('request made');
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World');
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
+const db = require('../lib/db');
+const template = require('../lib/template.js');
+const sanitizeHtml = require('sanitize-html');
+
+router.get('/create', (request, response) => {
+  db.query(`SELECT * FROM topic`, (error, topics) => {
+    if(error){
+      throw error;
+    }
+    db.query(`SELECT * FROM author`, (error2, authors) => {
+      if(error2){
+        throw error2;
+      }
+      const title = 'Create';
+      const list = template.list(topics);
+      const html = template.HTML(title, list, `
+        <form action="/comment/create_process" method="post">
+          <p><input type="text" name="comment" placeholder="comment"></p>
+          <p>
+            <select name="topic">
+              ${template.topicSelect(topics)}
+            </select>
+          </p>
+          <p>
+            <select name="author">
+              ${template.authorSelect(authors)}
+            </select>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+      `, '');
+      response.send(html);
+    });
+  });
 });
 
-// listen for requests on port 3000
-server.listen(3000, '
+router.post('/create_process', upload.single('comment'), (request, response) => {
+  const post = request.body;
+  db.query(`
+    INSERT INTO comment (comment, created, author_id, topic_id) 
+      VALUES(?, NOW(), ?, ?)`,
+    [post.comment, post.author, post.topic],
+    (error, result) => {
+      if(error){
+        throw error;
+      }
+      response.redirect(`/comment/${result.insertId}`);
+    }
+  );
+});
 
+router.get('/update/:commentId', (request, response) => {
+  const commentId = request.params.commentId;
+  db.query(`SELECT * FROM topic`, (error, topics) => {
+    if(error){
+      throw error;
+    }
+    db.query(`SELECT * FROM author`, (error2, authors) => {
+      if(error2){
+        throw error2;
+      }
+      db.query(`SELECT * FROM comment WHERE id=?`, [commentId], (error3, comment) => {
+        if(error3
